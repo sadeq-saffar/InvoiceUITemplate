1ee87cb986d3f0d3d1d1732a1a002347f90133d1
sadeq Saffar <ssaffar@gmail.com>
2023-07-26 18:17:33 +0330

    init

---
+++ loadPage.js
@@ -0,0 +1,25 @@
+function loadPage(url) {
+    fetch(url)
+      .then(response => response.text())
+      .then(html => {
+      console.log(html)
+  
+      document.getElementById('content').innerHTML = html;
+      })
+      .catch(error => console.error('Error loading page:', error));
+  }
+  
+  // loadPage('index.html');
+  
+  // const menuListItem =  document.querySelectorAll('.menu-list-item a')
+  // document.querySelectorAll('.menu-list-item a').forEach(link => {
+    // console.log(link.getAttribute('href'))
+  
+  // })
+  document.querySelectorAll('.menu-list-item a').forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      const url = link.getAttribute('href');
+      loadPage(url);
+    });
+  });
